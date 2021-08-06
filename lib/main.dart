8d6caf3b817b1804a5ba5a0446769280374da3b2
SBSC-DEV <aogundijo@sbsc.com>
2021-08-06 16:47:07 +0100

    custom gradient text implementation

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
           ),
           home: BottomNav(),
-          
+
         ),
       ),
       designSize: Size(375, 812),
