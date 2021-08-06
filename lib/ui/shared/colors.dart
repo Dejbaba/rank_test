01c11ec5c3832c0dfb88f92f419990415903fe1a
SBSC-DEV <aogundijo@sbsc.com>
2021-08-06 23:50:05 +0100

    added new colors

--- lib/ui/shared/colors.dart
+++ lib/ui/shared/colors.dart
@@ -5,5 +5,6 @@ const turquoiseBlue = Color(0xff20C1F3);
 const shaftGrey = Color(0xff333333);
 const spaceGrey = Color(0xff303437);
 const manateeGrey = Color(0xff8B90A1);
+const mercuryGrey = Color(0xffE5E5E5);
 
 
