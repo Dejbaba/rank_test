08544ee975888d15ced70523fa8ca82a461edb60
dejman <dejbaba@gmail.com>
2021-08-07 16:56:17 +0100

    added new dummy data

---
+++ lib/core/models/dummy_activity.dart
@@ -0,0 +1,9 @@
+import 'package:rank_task/core/models/dummy_user.dart';
+
+class DummyActivity{
+
+  final String date;
+  final List<DummyUser> activities;
+
+  DummyActivity({this.date, this.activities});
+}
