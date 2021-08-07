7a093e6ae4b3906e1cda069ff0c5f33e32373753
dejman <dejbaba@gmail.com>
2021-08-07 16:56:36 +0100

    custom widget added for back navigation

---
+++ lib/ui/views/widgets/back_navigator.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BackNavigator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: ()=> Navigator.pop(context),
+        child: Center(child: Image.asset("assets/images/back_arrow.png", height: 23.83.h, width: 30.6.w,)));
+  }
+}
