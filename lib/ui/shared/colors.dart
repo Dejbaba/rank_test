43cd437d9b3171224c7b6fbd58a9775d1e73af26
SBSC-DEV <aogundijo@sbsc.com>
2021-08-07 14:02:54 +0100

    added new colors

--- lib/ui/shared/colors.dart
+++ lib/ui/shared/colors.dart
@@ -6,5 +6,6 @@ const shaftGrey = Color(0xff333333);
 const spaceGrey = Color(0xff303437);
 const manateeGrey = Color(0xff8B90A1);
 const mercuryGrey = Color(0xffE5E5E5);
+const emperorGrey = Color(0xff4F4F4F);
 
 
